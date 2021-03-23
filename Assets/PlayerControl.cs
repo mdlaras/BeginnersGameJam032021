a48c4df15fb59d764e6c807aecea69fa34ea6371
mdlaras <mariadlarasati@gmail.com>
2021-03-23 09:38:10 +0700

    first commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localPosition += new Vector3(-5,0,0) * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localPosition += new Vector3(5,0,0) * Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.localPosition += new Vector3(0,100,0) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/PortalControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalControl : MonoBehaviour
+{
+    [SerializeField] bool isTransport;
+    [SerializeField] bool isChanger;
+    [SerializeField] bool isPortalIn;
+    [SerializeField] PortalControl portalOut;
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(isTransport && isPortalIn)
+        {
+            collision.gameObject.transform.localPosition = portalOut.gameObject.transform.position;
+        }
+        if(isChanger)
+        {
+            collision.gameObject.GetComponent<DimensionSwitch>().SwitchDimension();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
