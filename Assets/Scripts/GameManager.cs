20b4242bf84edc8fa7ca12eb1461bcd52835f635
Suyatna <zuyatna@gmail.com>
2021-03-24 14:48:10 +0700

    add manager (#2)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int dimensionShard;
+
+    private void Awake()
+    {
+        // if (SaveManager.Instance.hasLoaded)
+        // {
+        //     dimensionShard = SaveManager.Instance.activeSave.dimensionShard;
+        // }
+        // else
+        // {
+        //     SaveManager.Instance.activeSave.dimensionShard = dimensionShard;
+        // }
+
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameManager");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
