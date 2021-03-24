d5b86bfcab11add1cb86b793887c4de396d2a743
mdlaras <mariadlarasati@gmail.com>
2021-03-24 12:19:48 +0700

    make game menu as prefab

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+    public bool isActive = false;
+    void Start()
+    {
+       gameObject.SetActive(isActive); 
+    }
+
+    public void ToggleActive()
+    {
+        isActive = !isActive;
+        gameObject.SetActive(isActive);
+    }
+}
