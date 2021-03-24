1c5d901f9ac3495fb7286188c8531a7b79ce0b83
mdlaras <mariadlarasati@gmail.com>
2021-03-24 15:51:25 +0700

    make running animation in-game, except eyes

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -10,28 +10,45 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] Transform foot;
     [SerializeField] float footRadius;
     [SerializeField] LayerMask mask;
+    [SerializeField] SpriteRenderer eye;
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    
+    void AnimationControl()
+    {
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isMoving", false);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isMoving", true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        AnimationControl();
         isGrounded = Physics2D.OverlapCircle(foot.position, footRadius, mask) != null ? true : false;
        
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.localPosition += new Vector3(-velocity,0,0) * Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            eye.flipX = true;
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        else if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.localPosition += new Vector3(velocity,0,0) * Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            
         }
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpHeight), ForceMode2D.Force);
         }
+        
     }
 }
