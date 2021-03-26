3f4f4c7ef1be8d759c466e7e39a78ac90fa4fff6
Suyatna <zuyatna@gmail.com>
2021-03-26 18:00:28 +0700

    complete world map scene

---
+++ Assets/Scripts/Level/LevelStage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelStage : MonoBehaviour
+{
+    public int shardTotal;
+
+    public string sceneName;
+
+    public Button stageButton;
+
+    public Text textButton;
+
+    private void Start()
+    {
+        if (SaveManager.Instance.hasLoaded)
+        {
+            if (SaveManager.Instance.activeSave.dimensionShard >= shardTotal)
+            {
+                stageButton.interactable = true;
+            }
+
+            textButton.text = SaveManager.Instance.activeSave.dimensionShard + "/" + shardTotal;
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
