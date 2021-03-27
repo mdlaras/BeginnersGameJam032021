9fceab6c3a6449272c00a09c7c9857feed9d81ab
Suyatna <zuyatna@gmail.com>
2021-03-27 21:44:49 +0700

    reset dimensional shard when replay

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    private const string PlayerDataPrefKey = "PlayerData";
+    
+    private GameObject _gameManager;
+    
+    private void Awake()
+    {
+        _gameManager = GameObject.Find("GameManager");
+    }
+    
     public void RestartStage()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        _gameManager.GetComponent<GameManager>().dimensionShard = PlayerPrefs.GetInt(PlayerDataPrefKey);
     }
 
     public void GoToTitle()
